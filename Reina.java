f7a8f2d92ba1f903576f34a3cefd735568a6ba4b
diegorica <38293271+diegorica@users.noreply.github.com>
2018-06-01 19:12:22 +0200

    Reina.java
    q

--- Reina.java
+++ Reina.java
@@ -9,7 +9,72 @@ public class Reina extends Pieza {
 
 	public boolean posibleMovimiento(int filaCom, int columCom, int filaDest, int columDest)
 	{
-		return true;
+		boolean puedeMoverse = true;
+		TableroAjedrez TA = TableroAjedrez.getTableroAjedrez();
+		int sumadorFila;
+		int sumadorColum;
+		int colum = columCom;
+		int fila = filaCom;
+		if (filaCom==filaDest && columCom==columDest)
+		{
+			puedeMoverse = false;
+		}
+		else if (filaCom==filaDest || columCom==columDest)
+		{
+			puedeMoverse = true;
+		}
+		else if (!(filaCom==filaDest || columCom==columDest) && (Math.abs(filaCom-filaDest)==Math.abs(columCom-columDest)))
+		{
+			puedeMoverse = true;
+		}
+		else
+		{
+			puedeMoverse = false;
+		}
+		if (puedeMoverse)
+		{
+			if (filaDest>filaCom)
+			{
+				sumadorFila = 1;
+			}
+			else if (filaDest==filaCom)
+			{
+				sumadorFila = 0;
+			}
+			else
+			{
+				sumadorFila = -1;
+			}
+			if (columDest>columCom)
+			{
+				sumadorColum = 1;
+			}
+			else if (columDest==columCom)
+			{
+				sumadorColum = 0;
+			}
+			else
+			{
+				sumadorColum = -1;
+			}
+			
+			fila = fila + sumadorFila;
+			colum = colum + sumadorColum;
+			while ((fila!=filaDest || colum!=columDest) && puedeMoverse)
+			{
+				if (TA.obtenerCaja(colum, fila).getPieza()!=null)
+				{
+					puedeMoverse = false;
+				}
+				fila = fila + sumadorFila;
+				colum = colum + sumadorColum;
+			}
+			if (puedeMoverse && TA.obtenerCaja(colum, fila).getPieza()!=null && TA.obtenerCaja(colum, fila).getPieza().esDelMismoColor(TA.obtenerCaja(columCom, filaCom).getPieza()))
+			{
+				puedeMoverse = false;
+			}
+		}
+		return puedeMoverse;
 	}
 
 }
